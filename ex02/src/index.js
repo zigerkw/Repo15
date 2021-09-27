74c2d69ea1338c682be29721c12adc62c61bbd27
zigerkw <zejdhadzic@hotmail.com>
2021-09-27 15:45:07 +0300

    druga vjezba

---
+++ ex02/src/index.js
@@ -0,0 +1,23 @@
+function checkVariableScope() {
+    'use strict';
+
+    // only change code below this line
+    let i = 'function variable';
+    for (let i = 0; i < 3; i++)
+    // only change code above this line
+
+    if (true) {
+
+        // only change code below this line
+        i = 'block variable';
+        // only change code above this line
+
+        console.log('Scope i is: ', i);
+    }
+    console.log('Scope i is: ', i);
+    return i;
+}
+
+checkVariableScope();
+
+module.exports = checkVariableScope;
