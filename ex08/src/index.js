819b8597342ddc2ea1494f5f988c9a182793d0d7
zigerkw <zejdhadzic@hotmail.com>
2021-09-27 19:24:29 +0300

    osma vjezba

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+const sum = (num1, num2 = 1) => {
+
+return(num1 + num2);
+
+}
+// Only change code above this line
+
+console.log(sum(4, 6));  //Change this line
+
+module.exports = sum;
