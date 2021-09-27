cdab6ba7b37b1a153e3c55eddd640ec812b0547d
zigerkw <zejdhadzic@hotmail.com>
2021-09-27 17:15:58 +0300

    cetvrta vjezba

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+const milili = [10, 25, 4]; // DO NOT change this line
+
+function myArr(mili) {
+    'use strict';
+
+    // Only change code below this line
+    mili = [4, 10, 25];
+
+    // Only change code above this line
+    return mili;
+}
+console.log(myArr(milili));  // Change this line
+
+module.exports = myArr;
